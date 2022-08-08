740f733360b7ee1448cd1145c78d4b6373866d73
namhuynn1911 <namhuynh191193@gmail.com>
2022-08-08 17:10:58 +0700

    neww

---
+++ ss01_database/practice/database_2.sql
@@ -0,0 +1,25 @@
+-- create database my_name;
+-- create table my_name.poser(
+-- id int,
+-- name varchar(45),
+-- age int,
+-- primary key(id)
+-- );
+-- select*from my_name.poser;
+--  insert into poser(id,name,age)
+--  values ('2','huynh','29');
+-- select*from my_name.poser;
+-- truncate table poser;
+--  select*from my_name.poser;
+-- alter table poser
+-- add email varchar(45);
+-- alter table poser
+-- drop column email;
+-- truncate table poser;
+-- insert into poser(id,name,age)
+--  -- values ('2','huynh','29');
+-- --  insert into poser(id,name,age)
+--   values ('2','huynh','29');
+-- alter table poser
+-- modify id int;
+  select*from my_name.poser;
